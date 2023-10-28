d552be386ebee389ef21c79a31fe1b02d4d78f18
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-10-28 13:02:41 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { GlobalStyles } from './styles/Global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode basename="/vugar-gasimov/">
     <App />
-    <GlobalStyles />
   </React.StrictMode>
 );
